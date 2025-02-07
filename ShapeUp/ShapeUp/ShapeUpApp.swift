0984bbcc3d2389a8ad03a7ce2cb12a114d3914b5
ClayMorton <mmorton1163@gmail.com>
2025-02-07 16:46:25 -0500

    init repo

---
+++ ShapeUp/ShapeUp/ShapeUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapeUpApp.swift
+//  ShapeUp
+//
+//  Created by Clay Morton on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ShapeUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShapeUp/ShapeUpTests/ShapeUpTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShapeUpTests.swift
+//  ShapeUpTests
+//
+//  Created by Clay Morton on 2/7/25.
+//
+
+import Testing
+@testable import ShapeUp
+
+struct ShapeUpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
