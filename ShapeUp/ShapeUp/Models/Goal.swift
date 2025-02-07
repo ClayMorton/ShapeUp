838129a812182e02fc56d2bec93def2c4bcec34d
ClayMorton <mmorton1163@gmail.com>
2025-02-07 17:08:49 -0500

    creating a Goal Model

--- ShapeUp/ShapeUp/Models/Goal.swift
+++ ShapeUp/ShapeUp/Models/Goal.swift
@@ -5,3 +5,19 @@
 //  Created by Clay Morton on 2/7/25.
 //
 
+import Foundation
+
+class Goal: Identifiable, Codable {
+    var id = UUID()
+    var name: String
+    var description: String?
+    var icon: String
+    var checklist: [ChecklistItem]?
+    
+    init(name: String, description: String? = nil, icon: String, checklist: [ChecklistItem]? = nil) {
+        self.name = name
+        self.description = description
+        self.icon = icon
+        self.checklist = checklist
+    }
+}
