092586606229bef5dca8dfd473433dc90775cd98
ClayMorton <mmorton1163@gmail.com>
2025-02-07 17:02:17 -0500

    defining a checklist item

--- ShapeUp/ShapeUp/Models/ChecklistItem.swift
+++ ShapeUp/ShapeUp/Models/ChecklistItem.swift
@@ -5,3 +5,10 @@
 //  Created by Clay Morton on 2/7/25.
 //
 
+import Foundation
+
+struct ChecklistItem: Identifiable, Codable {
+    var id = UUID()
+    var title: String
+    var isCompleted: Bool = false
+}
